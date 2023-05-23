e336f919f388e555b8e91b67725e1c0b089f49f9
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-23 16:29:16 +0400

    Add files via upload

---
+++ FilesRead.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace FilesRead
+{
+    internal class FilesRead
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Users\Irakli\OneDrive\Desktop\C#\test\Names.txt";
+            StreamReader sr = new StreamReader(file);
+            Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+        }
+    }
+}
