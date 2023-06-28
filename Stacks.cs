73e124d13f00d9b4d635939ae8ab97000f51269f
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-28 15:25:55 +0400

    Add files via upload

---
+++ Stacks.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace Stacks
+{
+    internal class Stacks
+    {
+        static void Main(string[] args)
+        {
+            // creating
+            Stack stack = new Stack();
+
+            // adding
+            stack.Push("Davita");
+            stack.Push("Bacho");
+            stack.Push("Dama");
+            stack.Push("Irakla");
+            // removing
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
