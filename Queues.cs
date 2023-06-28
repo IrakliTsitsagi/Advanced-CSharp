473b0b4b820e127379658bee537b6d60fe9b563c
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-28 15:32:58 +0400

    Add files via upload

---
+++ Queues.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace Queues
+{
+    internal class Queues
+    {
+        static void Main(string[] args)
+        {
+           // pirveli shedis pirveli gamodis
+
+            // creating
+            Queue queue = new Queue();
+
+
+            // adding
+            queue.Enqueue("Davita");
+            queue.Enqueue("Dama");
+            queue.Enqueue("Sensei");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            // removing
+            Console.WriteLine("----------");
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
