7aa8a44fa2b37bcd77affd8be5bd09ee52164847
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-29 16:35:43 +0400

    Add files via upload

---
+++ KeyValuePairApp.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Channels;
+
+namespace KeyValuePairApp
+{
+    internal class KeyValuePairApp
+    {
+        static void Main(string[] args)
+        {
+            var capitals = new List<KeyValuePair<string, string>>();
+
+            capitals.Add(new KeyValuePair<string, string>("Georgia", "Tbilisi"));
+            capitals.Add(new KeyValuePair<string, string>("Russia", "Moscow"));
+            capitals.Add(new KeyValuePair<string, string>("Turkey", "Ankara"));
+            capitals.Add(new KeyValuePair<string, string>("Armenia", "Erevan"));
+
+            capitals.RemoveAt(1);
+
+            foreach (var item in capitals)
+            {
+                Console.WriteLine(item.Key);
+            }
+            Console.WriteLine(GetFirstAndLastName().Key + " " + GetFirstAndLastName().Value);
+        }
+
+
+        public static KeyValuePair<string, string> GetFirstAndLastName()
+        {
+            string firstName = "Dejan";
+            string lastName = "Stankovic";
+            return new KeyValuePair<string, string>(firstName, lastName);
+        }
+    }
+}
