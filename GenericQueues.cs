62819b7251b5ecb4922a3a68b253b30ec133fd98
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-29 16:20:15 +0400

    Add files via upload

---
+++ GenericQueues.cs
@@ -0,0 +1,31 @@
+﻿namespace GenericQueues
+{
+    internal class GenericQueues
+    {
+        static void Main(string[] args)
+        {
+            Queue<DateTime> arrivals = new Queue<DateTime>();
+
+            arrivals.Enqueue(new DateTime(2017, 01, 01));
+            arrivals.Enqueue(new DateTime(2019, 10, 27));
+            arrivals.Enqueue(new DateTime(2020, 06, 09));
+
+            Console.WriteLine("at the top " + arrivals.Peek());
+            Console.WriteLine("-------");
+
+            foreach (var item in arrivals)
+            {
+                Console.WriteLine(item);
+            }
+
+            arrivals.Dequeue(); // pirvels amoagdebs es
+
+            Console.WriteLine("at the top " + arrivals.Peek());
+            Console.WriteLine("-------");
+            foreach (var item in arrivals)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
