0c613ffb7778586b3d36b347c7bea971a212bd84
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-29 15:47:34 +0400

    Add files via upload

---
+++ GenericStacks.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericStacks
+{
+    internal class GenericStacks
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> Ram = new Stack<string>();
+
+            Ram.Push("Visual Studio");
+            Ram.Push("Photoshop");
+            Ram.Push("Google Chrome");
+            Ram.Push("NotePad");
+
+            Console.WriteLine(Ram.Peek());
+
+            //foreach (var item in Ram)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
