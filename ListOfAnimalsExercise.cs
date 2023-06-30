97ab033819ad309da2045caf79f50d2b3e925418
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-30 12:28:03 +0400

    Add files via upload

---
+++ ListOfAnimalsExercise.cs
@@ -0,0 +1,49 @@
+﻿namespace ListOfAnimalsExercise
+{
+    internal class ListOfAnimalsExercise
+    {
+        static void Main(string[] args)
+        {
+            Animals dog = new Animals();
+            dog.Name = "Iriska";
+
+            Animals cat = new Animals();
+            cat.Name = "Miako";
+
+            Animals elephant = new Animals();
+            elephant.Name = "Fofy";
+
+            List<Animals> list = new List<Animals>();
+            list.Add(dog);
+            list.Add(cat);
+            list.Add(elephant);
+
+            foreach (var item in list)
+            {
+                item.SayHi();
+                item.Feed();
+            }
+        }
+    }
+
+    class Animals
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine("Hello my name is " + name);
+        }
+
+        public void Feed()
+        {
+            Console.WriteLine(name + " is now eating");
+        }
+    }
+}
