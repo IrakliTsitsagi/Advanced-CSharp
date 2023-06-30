999ac15924a98baadf94b972945c9f7ea9f22a02
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-30 12:17:09 +0400

    Add files via upload

---
+++ NameValueCollectionApp.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Specialized;
+
+namespace NameValueCollectionApp
+{
+    internal class NameValueCollectionApp
+    {
+        static void Main(string[] args)
+        {
+            // creating 
+
+            NameValueCollection cities = new NameValueCollection();
+
+            // adding
+
+            cities.Add("Georgia", "Gori");
+            cities.Add("Georgia", "Tbilisi");
+            cities.Add("Georgia", "Kutaisi");
+            cities.Add("Georgia", "Batumi");
+            cities.Add("Georgia", "Khashuri");
+            cities.Add("Turkey", "Ankara");
+            cities.Add("Turkey", "Antalia");
+
+
+            // displaying
+
+            foreach (string item in cities)
+            {
+                Console.Write(item + ": ");
+                Console.WriteLine(cities[item]);
+            }
+
+            // set
+
+            cities.Set("Italy", "Venice");
+
+            // contains
+
+            Console.WriteLine(cities.HasKeys());
+
+            // remove
+
+            cities.Remove("Turkey");
+            Console.WriteLine("after deleting");
+
+            foreach (string item in cities)
+            {
+                Console.Write(item + ": ");
+                Console.WriteLine(cities[item]);
+            }
+            // remove all
+            // cities.Clear();
+        }
+    }
+}
