903ab1d3f92ae48e56c5c1e33cf304c2062a8159
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-11 15:48:36 +0400

    Add files via upload

---
+++ TuplesApp.cs
@@ -0,0 +1,22 @@
+﻿namespace TuplesApp
+{
+    internal class TuplesApp
+    {
+        static void Main(string[] args)
+        {
+            var empInfo = new Tuple<int, string, string, DateTime, bool>(100, "Jack", "William", new DateTime(2017, 10, 15), true);
+            Console.WriteLine(empInfo.Item1);
+            Console.WriteLine(empInfo.Item2);
+            Console.WriteLine(empInfo.Item3);
+            Console.WriteLine(((DateTime)(empInfo.Item4)).ToShortDateString());
+            Console.WriteLine(empInfo.Item5);
+            Console.WriteLine();
+
+            var studentInfo = Tuple.Create(5, "Giorgi", "Didebulidze", new DateTime(2003, 10, 27));
+            Console.WriteLine(studentInfo.Item1);
+            Console.WriteLine(studentInfo.Item2);
+            Console.WriteLine(studentInfo.Item3);
+            Console.WriteLine(studentInfo.Item4);
+        }
+    }
+}
