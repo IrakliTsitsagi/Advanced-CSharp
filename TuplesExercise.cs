90c0dec7232949f4d5f8f2e5bb795cb0e6986a24
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-11 17:35:25 +0400

    Add files via upload

---
+++ TuplesExercise.cs
@@ -0,0 +1,35 @@
+﻿namespace TuplesExercise
+{
+    internal class TuplesExercise
+    {
+        static void Main(string[] args)
+        {
+            var employeesInformation = new List<Tuple<int, string, string, DateTime>>();
+            // gza 1
+            employeesInformation.Add(new Tuple<int, string, string, DateTime>(3, "Dama", "Didebulidze", new DateTime(2017, 11, 25)));
+            // gza 2
+            employeesInformation.Add(Tuple.Create(1, "Guja", "Butxuzi", new DateTime(2015, 01, 01)));
+            employeesInformation.Add(Tuple.Create(4, "Nodar", "Inauri", new DateTime(2015, 01, 02)));
+            employeesInformation.Add(Tuple.Create(2, "Joyola", "Demurishvili", new DateTime(2015, 01, 09)));
+
+            foreach (var employee in employeesInformation)
+            {
+                Console.WriteLine($"{employee.Item1}, {employee.Item2}, {employee.Item3}, {employee.Item4}");
+            }
+            Console.WriteLine();
+
+            employeesInformation.Sort();
+            foreach (var employee in employeesInformation)
+            {
+                Console.WriteLine($"{employee.Item1}, {employee.Item2}, {employee.Item3}, {employee.Item4}");
+            }
+            Console.WriteLine();
+
+            employeesInformation.Reverse();
+            foreach (var employee in employeesInformation)
+            {
+                Console.WriteLine($"{employee.Item1}, {employee.Item2}, {employee.Item3}, {employee.Item4}");
+            }
+        }
+    }
+}
