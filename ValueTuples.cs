e9b776731ffdc514f71c10e9264385b8d598731c
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 11:34:11 +0400

    Add files via upload

---
+++ ValueTuples.cs
@@ -0,0 +1,24 @@
+﻿namespace ValueTuples
+{
+    internal class ValueTuples
+    {
+        static void Main(string[] args)
+        {
+            // ase davainstale value tuple PM> install-package "system.valuetuple" tools-nuget package manager console
+            int x = 0;
+
+            (int playerNo, string playerName, int playerGoals) ronaldo = (7, "Ronaldo", 55);
+            Console.WriteLine(ronaldo.playerNo);
+            Console.WriteLine(ronaldo.playerName);
+            Console.WriteLine(ronaldo.playerGoals);
+            Console.WriteLine();
+
+            // another way
+            var messi = (playerNo:10, playerName:"Messi", playerGoals:60);
+            Console.WriteLine(messi.playerNo);
+            Console.WriteLine(messi.playerName);
+            Console.WriteLine(messi.playerGoals);
+
+        }
+    }
+}
