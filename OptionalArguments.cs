8f2835b91bae33d353a2fb5f1420cec8619338c3
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 15:31:40 +0400

    Add files via upload

---
+++ OptionalArguments.cs
@@ -0,0 +1,32 @@
+﻿namespace OptionalArguments
+{
+    internal class OptionalArguments
+    {
+        static void Main(string[] args)
+        {
+            DisplayPlayerInfo(77, "Kvaratskhelia", 90, "Georgia", "Napoli");
+            DisplayPlayerInfo(10, "Messi", 89, "Argentina");
+            DisplayPlayerInfo(11, "Nkunku", 10); // country ze unknown dawers clubze arafers
+        }
+
+        public static void DisplayPlayerInfo(int no, string name, int goals, string country = "Unknown", string club = "")
+        {
+            Console.WriteLine("Hi from the first method");
+            Console.WriteLine(no);
+            Console.WriteLine(name);
+            Console.WriteLine(goals);
+            Console.WriteLine(country);
+            Console.WriteLine(club);
+            Console.WriteLine();
+        }
+
+        public static void DisplayPlayerInfo(int no, string name, int goals)
+        {
+            Console.WriteLine("Hi from the overloaded method");
+            Console.WriteLine(no);
+            Console.WriteLine(name);
+            Console.WriteLine(goals);
+            Console.WriteLine();
+        }
+    }
+}
