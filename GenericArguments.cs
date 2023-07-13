7838d0a773c354516b6436a48324cbcdde7f95f5
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 15:51:05 +0400

    Add files via upload

---
+++ GenericArguments.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericArguments
+{
+    internal class GenericArguments
+    {
+        static void Main(string[] args)
+        {
+            DisplayInfo("John");
+            DisplayInfo(2);
+            DisplayInfo(2.5);
+            DisplayInfo(DateTime.Now);
+
+            var playerInfo = (7, "ylekaca", 10);
+            DisplayInfo(playerInfo);
+        }
+
+        // yvelaferze midis int string shmrint print T magaria
+        public static void DisplayInfo<T>(T info)
+        {
+            Console.WriteLine(info);
+        }
+    }
+}
