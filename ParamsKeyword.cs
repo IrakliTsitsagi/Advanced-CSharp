0fab213574290d4c5027d482e9fae6cc479ee3c3
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 16:01:00 +0400

    Add files via upload

---
+++ ParamsKeyword.cs
@@ -0,0 +1,20 @@
+﻿namespace ParamsKeyword
+{
+    internal class ParamsKeyword
+    {
+        static void Main(string[] args)
+        {
+            LoopThroughArray(new string[5] { "Dejan", "Bejan", "Ylekac", "Slesha", "Lado" }); // aq 5 unda iyos imena
+            Console.WriteLine();
+            LoopThroughArray("Yursha", "ufroyle", "uaresiyle"); // aq ramdensac minda chavwer axla
+        }
+
+        public static void LoopThroughArray(params string[] names) // params resizes aketebs tavisit kaia
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
