98e45bc5a0706fb4164a0dc235580fa5f7391939
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 14:58:52 +0400

    Add files via upload

---
+++ RecursiveMethodsExercise.cs
@@ -0,0 +1,23 @@
+﻿namespace RecursiveMethodsExercise
+{
+    internal class RecursiveMethodsExercise
+    {
+        static void Main(string[] args)
+        {
+            string path = string.Empty;
+            path = "C:\\Users\\Irakli\\OneDrive\\Desktop\\C#\\Recursive";
+            Console.WriteLine(path);
+            DisplayFolder(path, 0);
+        }
+
+        public static void DisplayFolder(string path, int indent)
+        {
+            foreach (var folder in Directory.GetDirectories(path))
+            {
+                Console.WriteLine($"{new string(' ', indent)} {Path.GetFileName(folder)}");
+                DisplayFolder(folder, indent + 2);
+                // es indent + 2 aris folders shiga folderis saxeli ro gawios gverdze 
+            }
+        }
+    }
+}
