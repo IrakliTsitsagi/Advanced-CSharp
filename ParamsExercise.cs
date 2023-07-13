1a26346efabb0fe02a9c5cb6e9acffe9f01484c1
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 16:34:46 +0400

    Add files via upload

---
+++ ParamsExercise.cs
@@ -0,0 +1,23 @@
+﻿namespace ParamsExercise
+{
+    internal class ParamsExercise
+    {
+        static void Main(string[] args)
+        {
+            loopThroughArray(10, 50, 25, 3);
+
+            loopThroughArray("Ahmada", "Gela", "Vaja");
+
+            loopThroughArray(DateTime.Now, new DateTime(2017, 10, 09));
+        }
+
+        public static void loopThroughArray<T>(params T[] items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
