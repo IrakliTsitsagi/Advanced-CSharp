952312d1e512a8dedc1a8fd75c86fb449e7fa36d
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 11:42:56 +0400

    Add files via upload

---
+++ NestedValueTuple.cs
@@ -0,0 +1,18 @@
+﻿namespace NestedValueTuple
+{
+    internal class NestedValueTuple
+    {
+        static void Main(string[] args)
+        {
+            var ronaldoGoals = (year2017: 64, year2016: 60, year2015: 50);
+            var ronaldo = (playerNo: 10, playerName: "Ronaldo", playerGoals:ronaldoGoals);
+            Console.WriteLine(ronaldo.playerNo);
+            Console.WriteLine(ronaldo.playerName);
+            Console.WriteLine(ronaldo.playerGoals.year2017);
+            Console.WriteLine(ronaldo.playerGoals.year2016);
+            Console.WriteLine(ronaldo.playerGoals.year2015);
+            Console.WriteLine();
+
+        }
+    }
+}
