c2447574526762ce37b4a2ddcdae77a7487d0464
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 16:52:28 +0400

    Add files via upload

---
+++ Nested_Local_Functions.cs
@@ -0,0 +1,35 @@
+﻿namespace Nested_Local_Functions
+{
+    internal class Nested_Local_Functions
+    {
+        static void Main(string[] args)
+        {
+            PerformMathOperations(1, 2, 3, 4);
+        }
+
+        public static void PerformMathOperations(params int[] numbers)
+        {
+            Console.WriteLine("Add = " + AddNumbers());
+            int AddNumbers()
+            {
+                int result = 0;
+                foreach (var number in numbers)
+                {
+                    result = result + number;
+                }
+                return result;
+            }
+
+            Console.WriteLine("Multi = " + MultiplyNumbers());
+            int MultiplyNumbers()
+            {
+                int result = 1;
+                foreach (var number in numbers)
+                {
+                    result = result * number;
+                }
+                return result;
+            }
+        }
+    }
+}
