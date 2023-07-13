68e3b7751e4c143821957f7e8f4310e6ee2c5c17
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 15:39:28 +0400

    Add files via upload

---
+++ NamedArguments.cs
@@ -0,0 +1,19 @@
+﻿namespace NamedArguments
+{
+    internal class NamedArguments
+    {
+        static void Main(string[] args)
+        {
+            DisplayPlayerInfo(country: "Georgia", name: "Khvicha", goals: 90, no: 77);
+        }
+
+        public static void DisplayPlayerInfo(int no, string name, int goals, string country = "Unknown")
+        {
+            Console.WriteLine(no);
+            Console.WriteLine(name);
+            Console.WriteLine(goals);
+            Console.WriteLine(country);
+            Console.WriteLine();
+        }
+    }
+}
