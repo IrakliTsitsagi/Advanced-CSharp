02d1c791c9b40be2ad9192d2dae2be2f2752e3eb
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 14:47:52 +0400

    Add files via upload

---
+++ RecursiveMethodsApp.cs
@@ -0,0 +1,36 @@
+﻿namespace RecursiveMethodsApp
+{
+    internal class RecursiveMethodsApp
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+
+            Console.WriteLine("Non recursive way");
+            Console.WriteLine(FactorialNoNRecursive(number));
+            Console.WriteLine("Recursive way");
+            Console.WriteLine(Factorial(number));
+        }
+
+        // Non recursive way
+        public static int FactorialNoNRecursive(int n)
+        {
+            if (n == 0) return 1;
+            int value = 1;
+            for (int i = n; i > 0; i--)
+            {
+                value *= i;
+                Console.WriteLine(i);
+            }
+            return value;
+        }
+
+        // Recursive way
+
+        public static int Factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
