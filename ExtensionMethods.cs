8d8ece380f29c9fbe84f246eee6555a83b7f3e0f
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 17:18:09 +0400

    Add files via upload

---
+++ ExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿namespace ExtensionMethods
+{
+    internal class ExtensionMethods
+    {
+        static void Main(string[] args)
+        {
+            int x = 90;
+            Console.WriteLine(x.IsGreater(50));
+
+            string text = "Ahmad";
+            Console.WriteLine(text.IsNumber()); // tu ricxvebs chavwer textshi true tu asoebs false
+        }
+    }
+
+    // aucilebelia statikuri klasi
+    static class MyCustomExtension
+    {
+        public static bool IsGreater(this int value, int number)
+        {
+            return value > number;
+        }
+
+        public static bool IsNumber(this string text)
+        {
+            return int.TryParse(text, out int result);
+        }
+    }
+}
