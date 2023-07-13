01b5f56b7b7e2cabdf30029d52d0eb67fc4d62f1
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-13 11:59:53 +0400

    Add files via upload

---
+++ ValueTuplesWithMethods.cs
@@ -0,0 +1,30 @@
+﻿namespace ValueTuplesWithMethods
+{
+    internal class ValueTuplesWithMethods
+    {
+        static void Main(string[] args)
+        {
+            var penaldo = (7, "Penaldo", 60);
+            var messi = (10, "Messi", 69);
+
+            DisplayPlayerInfo(penaldo);
+            DisplayPlayerInfo(messi);
+
+            DisplayPlayerInfo(DisplayPlayerInfo());
+        }
+
+        public static void DisplayPlayerInfo((int playerNo, string playerName, int playerGoal) player)
+        {
+            Console.WriteLine(player.playerNo);
+            Console.WriteLine(player.playerName);
+            Console.WriteLine(player.playerGoal);
+            Console.WriteLine();
+        }
+
+        public static (int, string, int) DisplayPlayerInfo()
+        {
+            var gerrard = (10, "Gerrard", 65);
+            return gerrard;
+        }
+    }
+}
