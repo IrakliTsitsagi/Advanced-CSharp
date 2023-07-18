c93afef44e339d3e5eb5aa9e85a6e0960736935d
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-18 13:08:08 +0400

    Add files via upload

---
+++ DelegatesVsDirectMethodCalls.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace DelegatesVsDirectMethodCalls
+{
+    public delegate void DoSomethingDelegate();
+    internal class DelegatesVsDirectMethodCalls
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watchOne = new Stopwatch();
+            Stopwatch watchTwo = new Stopwatch();
+            DoSomethingDelegate taskOne = new DoSomethingDelegate(DoSomething);
+
+            // using delegates
+            Console.WriteLine("delegates method\n ---------------");
+            watchOne.Start();
+            taskOne();
+            watchOne.Stop();
+            var elapsedOne = watchOne.Elapsed;
+            Console.WriteLine(Math.Round(elapsedOne.TotalSeconds, 2));
+
+
+            // using direct calls 
+            Console.WriteLine("\nDirect method\n ---------------");
+            watchTwo.Start();
+            DoSomething();
+            watchTwo.Stop();
+            var elapsedTwo = watchOne.Elapsed;
+            Console.WriteLine(Math.Round(elapsedTwo.TotalSeconds, 2));
+
+            Console.WriteLine("\nDelegates saved you " + Math.Round(elapsedTwo.TotalMilliseconds - elapsedOne.TotalMilliseconds, 4) + " Milliseconds");
+            Console.WriteLine("\nDelegates saved you " + Math.Round(elapsedTwo.TotalSeconds - elapsedOne.TotalSeconds, 4) + " Seconds");
+
+        }
+
+        public static void DoSomething()
+        {
+            double number = 8.54321;
+            List<double> list = new List<double>();
+            for (double i = 0; i < 60_000_000; i++)
+            {
+                number += i + DateTime.Now.Second;
+                list.Add(number);
+            }
+            list.Reverse();
+        }
+    }
+}
