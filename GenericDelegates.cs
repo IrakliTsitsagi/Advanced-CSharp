7da07c4e1be2bf5c4830b46ecc259f8f20168ed3
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-19 16:17:56 +0400

    Add files via upload

---
+++ GenericDelegates.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+
+namespace GenericDelegates
+{
+    public delegate T DisplayInfo<T>(T value);
+    internal class GenericDelegates
+    {
+        static void Main(string[] args)
+        {
+            DisplayInfo<int> myNumber = new DisplayInfo<int>(DisplayValue);
+            Console.WriteLine(myNumber(100));
+
+            DisplayInfo<double> myDoubleNumber = new DisplayInfo<double>(DisplayValue);
+            Console.WriteLine(myDoubleNumber(3.5));
+
+            DisplayInfo<DateTime> myDate = new DisplayInfo<DateTime>(DisplayValue);
+            Console.WriteLine(myDate(new DateTime(2010, 2, 28)));
+        }
+
+        public static T DisplayValue<T>(T value)
+        {
+            Console.WriteLine("Now we are accessing variable of type " + value.GetType().Name);
+            return value;
+        }
+    }
+}
