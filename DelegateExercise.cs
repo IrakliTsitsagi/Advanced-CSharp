1d97ce0f537e10f12fd6382c99bce2e4152e275d
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-19 15:41:34 +0400

    Add files via upload

---
+++ DelegateExercise.cs
@@ -0,0 +1,26 @@
+﻿namespace DelegateExercise
+{
+    public delegate void DisplayPlayer(Dictionary<int, string> players);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary <int,string> listOfPlayers = new Dictionary<int, string> ();
+            listOfPlayers.Add(7, "Penaldo");
+            listOfPlayers.Add(10, "Messi");
+            listOfPlayers.Add(77, "Kvara");
+
+            DisplayPlayer player = new DisplayPlayer(DisplayPlayerInfo);
+            player(listOfPlayers);
+          
+        }
+
+        public static void DisplayPlayerInfo(Dictionary<int,string> players)
+        {
+            foreach (var player in players)
+            {
+                Console.WriteLine($"Player no : {player.Key}, Player name : {player.Value}"); ;
+            }
+        }
+    }
+}
