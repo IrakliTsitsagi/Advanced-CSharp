43b1d852cbb79912478062ef7d9a5ca2d7d2397e
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-19 15:53:11 +0400

    Add files via upload

---
+++ MulticastDelegates.cs
@@ -0,0 +1,37 @@
+﻿namespace MulticastDelegates
+{
+    public delegate void SayHiDelegate();
+    internal class MulticastDelegates
+    {
+        static void Main(string[] args)
+        {
+            SayHiDelegate sayHi = null;
+
+            sayHi = new SayHiDelegate(SayHiEnglish);
+            sayHi += new SayHiDelegate(SayHiGeorgian);
+            sayHi += new SayHiDelegate(SayHiSpanish);
+
+            sayHi();
+        }
+
+        // say hi in English
+        public static void SayHiEnglish()
+        {
+            Console.WriteLine("Hello");
+        }
+
+
+        // say hi in English
+        public static void SayHiGeorgian()
+        {
+            Console.WriteLine("Gamarjoba");
+        }
+
+
+        // say hi in English
+        public static void SayHiSpanish()
+        {
+            Console.WriteLine("Hola");
+        }
+    }
+}
