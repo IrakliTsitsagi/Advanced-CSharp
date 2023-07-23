8470f8f3740a16db49c9dad52dcc9503ea7acdf3
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-23 18:18:35 +0400

    Add files via upload

---
+++ Events.cs
@@ -0,0 +1,35 @@
+﻿namespace Events
+{
+    public delegate void myDelegate(string name);
+    internal class Events
+    {
+        public event myDelegate myEvent;
+
+        public Events()
+        {
+            myEvent += new myDelegate(SayHiGeorgian);
+            myEvent += new myDelegate(SayHiItalian);
+            myEvent += new myDelegate(SayHiEnglish);
+        }
+        static void Main(string[] args)
+        {
+            Events prog = new Events();
+            prog.myEvent("Guram");
+        }
+
+        public void SayHiEnglish(string name)
+        {
+            Console.WriteLine("Hi " + name);
+        }
+
+        public void SayHiGeorgian(string name)
+        {
+            Console.WriteLine("Gamarjoba " + name);
+        }
+
+        public void SayHiItalian(string name)
+        {
+            Console.WriteLine("Ciao " + name);
+        }
+    }
+}
