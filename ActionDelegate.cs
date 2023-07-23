3ec03d0b458c2283371368fab6c76a4e20c07fd2
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-23 17:35:03 +0400

    Add files via upload

---
+++ ActionDelegate.cs
@@ -0,0 +1,26 @@
+﻿namespace ActionDelegate
+{
+    internal class ActionDelegate
+    {
+        static void Main(string[] args)
+        {
+            // generic
+            Action<int> actionOne = DisplayInformation;
+            actionOne(16);
+
+            // non generic
+            Action actionTwo = DisplayInformation;
+            actionTwo();
+        }
+
+        public static void DisplayInformation(int number)
+        {
+            Console.WriteLine("Dejana aris am nomrit " + number);
+        }
+
+        public static void DisplayInformation()
+        {
+            Console.WriteLine("Mgelkaca");
+        }
+    }
+}
