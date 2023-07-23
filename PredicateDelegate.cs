a6a8a628b841f6f4f8535760ee7e236b44467e6f
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-23 17:44:54 +0400

    Add files via upload

---
+++ PredicateDelegate.cs
@@ -0,0 +1,22 @@
+﻿namespace PredicateDelegate
+{
+    internal class Program
+    {
+        // eseigi funci unda areturnebdes actioni ara da predicates input parametri unda
+        //qondes da booleans areturnebdes
+        static void Main(string[] args)
+        {
+            Predicate<int> condition = IsAdmin;
+            Console.WriteLine(condition(10));
+        }
+
+        public static bool IsAdmin(int empNo)
+        {
+            if (empNo == 10)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
