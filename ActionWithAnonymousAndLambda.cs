f45591844dc9b01923cc5e00599021e6bb54e804
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-23 18:08:03 +0400

    Add files via upload

---
+++ ActionWithAnonymousAndLambda.cs
@@ -0,0 +1,29 @@
+﻿namespace ActionWithAnonymousAndLambda
+{
+    internal class ActionWithAnonymousAndLambda
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jack";
+
+            // Action
+            Action<string> Message = ShowMessage;
+            Message(name);
+
+            // Anonymous method
+            name = "John";
+            Action<string> anonymousMethod = delegate (string myName) { Console.WriteLine($"Hi, how are you today, {myName}?"); };
+            anonymousMethod(name);
+
+            // Lambda expression
+            name = "Sandrai";
+            Action<string> lambdaExpression = x => Console.WriteLine($"Hi, how are you today, {x}?");
+            lambdaExpression(name);
+        }
+
+        public static void ShowMessage(string name)
+        {
+            Console.WriteLine($"Hi, how are you today, {name}?");
+        }
+    }
+}
