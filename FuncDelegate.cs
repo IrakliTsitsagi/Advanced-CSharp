e89f61b41442b50a75c9a54d10fcd21d433d6718
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-23 17:29:00 +0400

    Add files via upload

---
+++ FuncDelegate.cs
@@ -0,0 +1,30 @@
+﻿namespace FuncDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> funcOne = AddTwoNumbers;
+            Console.WriteLine(AddTwoNumbers(3, 9));
+
+            Func<int> funcTwo = AddTwoNumbers;
+            Console.WriteLine(funcTwo());
+        }
+
+        public static int AddTwoNumbers(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int AddTwoNumbers()
+        {
+            int z = 0;
+            int x = 5;
+            int y = 2;
+
+            z = x + y;
+
+            return z;
+        }
+    }
+}
