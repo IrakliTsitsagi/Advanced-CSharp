101a6f75995c3ebd05aa8f7975f5988570cf41c1
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-23 17:55:21 +0400

    Add files via upload

---
+++ FuncWithAnonymousAndLambda.cs
@@ -0,0 +1,31 @@
+﻿namespace FuncWithAnonymousAndLambda
+{
+    internal class FuncWithAnonymousAndLambda
+    {
+        static void Main(string[] args)
+        {
+            string name = "Dejan Stankovichi";
+
+            // Func
+            Func<string, string> ConvertToUppercase = UppercaseString;
+            Console.WriteLine("Using func ");
+            Console.WriteLine(ConvertToUppercase(name));
+
+            // Anonymous method
+            Func<string, string> anonymousMethod = delegate (string myName) { return myName.ToUpper(); };
+            Console.WriteLine("Using anonymous method");
+            Console.WriteLine(anonymousMethod(name));
+
+            // Lambda expression
+            Func<string, string> lambdaExpression = text => text.ToUpper();
+            Console.WriteLine("Using lambda expression");
+            Console.WriteLine(lambdaExpression(name));
+
+        }
+
+        public static string UppercaseString(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
