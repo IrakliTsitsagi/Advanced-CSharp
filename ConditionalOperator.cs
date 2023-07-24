b59f740dd1a041742c83bb95b28f865ac406fcf6
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-24 19:46:55 +0400

    Add files via upload

---
+++ ConditionalOperator.cs
@@ -0,0 +1,25 @@
+﻿namespace ConditionalOperator
+{
+    internal class ConditionalOperator
+    {
+        static void Main(string[] args)
+        {
+            //if (IsAdmin() == true)
+            //{
+            //    Console.WriteLine("Yes, he is an admin");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not an admin");
+            //}
+
+            // shorter way
+            Console.WriteLine(IsAdmin() ? "Yes an admin" : "Not an admin");
+        }
+
+        public static bool IsAdmin()
+        {
+            return false;
+        }
+    }
+}
