43d3a5c586441109b373ef6eac9990c83f2a035a
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-24 15:54:02 +0400

    Add files via upload

---
+++ TimerApp.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace TimerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 3000;
+            timer.Enabled = true;
+
+            Console.WriteLine("Press x to exit");
+            while (Console.Read() != 'x') ;
+        }
+
+        private static void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Yes it is working " + DateTime.Now.ToLongTimeString());
+        }
+    }
+}
