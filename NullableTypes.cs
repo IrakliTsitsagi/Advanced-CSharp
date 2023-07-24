fb9049693ab75df8df61788540eaa1ea33659d04
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-24 16:46:33 +0400

    Add files via upload

---
+++ NullableTypes.cs
@@ -0,0 +1,32 @@
+﻿namespace NullableTypes
+{
+    internal class NullableTypes
+    {
+        static void Main(string[] args)
+        {
+            // DateTime myDate = new DateTime();
+            // Console.WriteLine(myDate);
+
+            DateTime? myDate = null; // null ro mivanicho --> ? unda dasva iq
+            Console.WriteLine(myDate);
+
+            // int number = 0;
+            int? number = null;
+            Console.WriteLine(number);
+            number = 5;
+
+            int numberTwo = (int)number;
+            numberTwo = 10;
+
+            Console.WriteLine(number ?? numberTwo);
+
+            string firstName = null;
+            firstName = "John";
+
+            string message = null;
+            message = "first name is null";
+
+            Console.WriteLine(firstName ?? message);
+        }
+    }
+}
