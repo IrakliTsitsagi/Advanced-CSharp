07b6245d8f16d5f4ba67546d79b6ca056a417e5d
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-24 19:50:59 +0400

    Add files via upload

---
+++ ConditionalOperatorExercise.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Channels;
+
+namespace ConditionalOperatorExercise
+{
+    internal class ConditionalOperatorExercise
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number % 2 == 0 ? "Even Number" : "Odd Number");
+        }
+    }
+}
