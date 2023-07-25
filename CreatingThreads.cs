a68d0e65586a80c49f3c096052813c5794e0282c
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-25 12:51:22 +0400

    Add files via upload

---
+++ CreatingThreads.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+
+namespace CreatingThreads
+{
+    internal class CreatingThreads
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(SayHiEnglish));
+            t1.Name = "thread number 1 (English)";
+            t1.Start();
+
+            Thread t2 = new Thread(new ThreadStart(SayHiSpanish));
+            t2.Name = "thread number 2(Spanish)";
+            t2.Start();
+        }
+
+        public static void SayHiEnglish()
+        {
+            Console.WriteLine("starting to execute " + Thread.CurrentThread.Name);
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(i + "Hi");
+            }
+        }
+
+        public static void SayHiSpanish()
+        {
+            Console.WriteLine("starting to execute " + Thread.CurrentThread.Name);
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(i + "Hola");
+            }
+        }
+    }
+}
