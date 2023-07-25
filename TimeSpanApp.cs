3703a0f865979043674c9dfdc54dbe9c43c89b44
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-25 12:31:38 +0400

    Add files via upload

---
+++ TimeSpanApp.cs
@@ -0,0 +1,23 @@
+﻿namespace TimeSpanApp
+{
+    internal class TimeSpanApp
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan ts = new TimeSpan(10, 8, 55);
+            Console.WriteLine(ts);
+            Console.WriteLine(ts.Days);
+            Console.WriteLine(ts.TotalHours);
+
+            ts = ts.Add(new TimeSpan(0, 0, 30));
+            Console.WriteLine(ts);
+
+            DateTime myDateOne = new DateTime(1999, 10, 30);
+            DateTime myDateTwo = new DateTime(2017, 10, 30);
+
+            TimeSpan sub = myDateTwo - myDateOne;
+            sub = myDateTwo.Subtract(myDateOne);
+            Console.WriteLine(sub.TotalDays / 365.25);
+        }
+    }
+}
