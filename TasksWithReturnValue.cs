0e70def066a0c35725d6bbbfe5a2ad74a59e7919
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-26 15:05:59 +0400

    Add files via upload

---
+++ TasksWithReturnValue.cs
@@ -0,0 +1,22 @@
+﻿namespace TasksWithReturnValue
+{
+    internal class TasksWithReturnValue
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = new Task<int>(() =>
+            {
+                int result = 0;
+                for (int i = 0; i <= 5; i++)
+                {
+                    result += i;
+                    Console.WriteLine(i);
+                }
+                return result;
+            });
+            task.Start();
+            Console.WriteLine(task.Result);
+            Console.ReadLine();
+        }
+    }
+}
