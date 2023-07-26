5177c640646d4833a6016e055e7b657b39655f33
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-26 15:54:53 +0400

    Add files via upload

---
+++ CheckingTasksStatus.cs
@@ -0,0 +1,42 @@
+﻿using System.Timers;
+using System.Threading.Tasks;
+using Timer = System.Timers.Timer;
+namespace CheckingTasksStatus
+{
+    internal class CheckingTasksStatus
+    {
+        static Timer timer;
+        static Task task;
+        static void Main(string[] args)
+        {
+            task = new Task(() =>
+           {
+               for (int i = 0; i <= 500; i++)
+               {
+                   Console.WriteLine(i);
+               }
+           });
+            task.Start();
+
+            timer = new Timer();
+            timer.Elapsed += CheckTheTask; // esec += mere tabi
+            timer.Interval = 100;
+            timer.Enabled = true;
+
+            Console.ReadLine();
+        }
+
+        private static void CheckTheTask(object sender, ElapsedEventArgs e)
+        {
+            if (task.Status == TaskStatus.Running)
+            {
+                Console.WriteLine("Task is running");
+            }
+            else if (task.Status == TaskStatus.RanToCompletion)
+            {
+                Console.WriteLine("Task is complete");
+                timer.Enabled = false;
+            }
+        }
+    }
+}
