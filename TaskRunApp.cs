e7ee034f731811ca3b7b188f22abd72dcefd5ef7
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-26 16:28:12 +0400

    Add files via upload

---
+++ TaskRunApp.cs
@@ -0,0 +1,40 @@
+﻿namespace TaskRunApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task.Run(() =>
+            //{
+            //    Console.WriteLine("Hi from task");
+            //}); // amas marjvena knopka da natura extract method izavs amas
+            SayHiTask();
+
+            AddNumbersTask(1, 2, 12);
+
+            Console.ReadLine();
+        }
+
+        private static Task SayHiTask()
+        {
+            return Task.Run(() =>
+              {
+                  Console.WriteLine("Hi from task");
+              });
+        }
+
+        public static Task AddNumbersTask(params int[] numbers)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("Add numbers task started");
+                int result = 0;
+                foreach (var number in numbers)
+                {
+                    result += number;
+                }
+                Console.WriteLine("Total " + result);
+            });
+        }
+    }
+}
