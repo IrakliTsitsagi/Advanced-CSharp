5eae0e8edaad65a42bb923c81b7bbbfc11704967
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-26 11:52:56 +0400

    Add files via upload

---
+++ ThreadExcersise2.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace ThreadExcersise2
+{
+    internal class ThreadExcersise2
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(delegate ()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Hello from anonymous assigned to t1");
+                }
+            }));
+            t1.Start();
+        }
+    }
+}
