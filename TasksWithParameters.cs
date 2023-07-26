171a2a8e6e75671427364c5d50a8d3775e7a4eb9
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-26 14:46:07 +0400

    Add files via upload

---
+++ TasksWithParameters.cs
@@ -0,0 +1,27 @@
+﻿namespace TasksWithParameters
+{
+    internal class TasksWithParameters
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(new Action<object>(SayHi), "Zaza");
+            task1.Start();
+
+            Task task2 = new Task(delegate (object name)
+            {
+                Console.WriteLine("Welcome back " + name);
+            }, "Dejan");
+            task2.Start();
+
+            Task task3 = new Task(name => { Console.WriteLine("Welcome back " + name); }, "Gubaz");
+            task3.Start();
+
+            Console.ReadLine();
+        }
+
+        public static void SayHi(object name)
+        {
+            Console.WriteLine("Welcome back " + name);
+        }
+    }
+}
