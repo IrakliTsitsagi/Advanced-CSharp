cee7db3337479e48b9c157829a78e82abf357639
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-26 16:04:48 +0400

    Add files via upload

---
+++ CancellingTasks.cs
@@ -0,0 +1,36 @@
+﻿namespace CancellingTasks
+{
+    internal class CancellingTasks
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancel = new CancellationTokenSource();
+            CancellationToken token = cancel.Token;
+
+            Task task = new Task(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Task is cancelled");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine(i + 1);
+                    }                  
+                }
+                Console.WriteLine("Task is Complete");
+            });
+            
+            task.Start();
+
+            // Thread.Sleep(1000);
+            // cancel.Cancel();
+            cancel.CancelAfter(1000); // ase jobs
+
+            Console.ReadLine();
+        }
+    }
+}
