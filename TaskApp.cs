60635c87a7b3e2dad7e8b0d75e1a20b905695daa
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-26 13:00:00 +0400

    Add files via upload

---
+++ TaskApp.cs
@@ -0,0 +1,46 @@
+﻿namespace TaskApp
+{
+    internal class TaskApp
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task(new Action(SayHi));
+            task.Start();
+
+            Task task2 = new Task(delegate ()
+            {
+                Console.WriteLine("Task2 is starting");
+                Console.WriteLine("Task2 is running");
+                Console.WriteLine("Hi there from task 2");
+                Thread.Sleep(4000);
+                Console.WriteLine("Task2 is complete");
+            });
+            task2.Start();
+
+            Task task3 = new Task(() =>
+            {
+                Console.WriteLine("Task3 is starting");
+                Console.WriteLine("Task3 is running");
+                Console.WriteLine("Hi there from task 3");
+                Thread.Sleep(5000);
+                Console.WriteLine("Task3 is complete");
+            });
+            task3.Start();
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Welcome back " + name);
+
+            Console.ReadLine();
+        }
+
+        public static void SayHi()
+        {
+            Console.WriteLine("Task is starting");
+            Console.WriteLine("Task is running");
+            Console.WriteLine("Hi there");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task is complete");
+        }
+    }
+}
