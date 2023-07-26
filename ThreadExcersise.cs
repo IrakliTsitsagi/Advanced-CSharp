9a4903ce0fa8025e19664b380548288a9152e65c
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-26 11:48:23 +0400

    Add files via upload

---
+++ ThreadExcersise.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Channels;
+
+namespace ThreadExcersise
+{
+    internal class ThreadExcersise
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(() => Console.WriteLine("Hello from Lambda")));
+            t1.Start();
+        }
+    }
+}
