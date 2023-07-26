cacfd2b9140b149e8e019f0d035143619a56dc3e
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-07-26 12:22:26 +0400

    Add files via upload

---
+++ ParameterizedThreadApp.cs
@@ -0,0 +1,23 @@
+﻿namespace ParameterizedThreadApp
+{
+    internal class ParameterizedThreadApp
+    {
+        static void Main(string[] args)
+        {
+            Helper helper = new Helper();
+            Thread t1 = new Thread(new ParameterizedThreadStart(helper.Loop));
+            t1.Start(100);
+        }
+
+        class Helper
+        {
+            public void Loop(object number)
+            {
+                for (int i = 0; i < int.Parse(number.ToString()); i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
