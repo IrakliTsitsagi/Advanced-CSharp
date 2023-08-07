9f565f7d9c7e3709900aa245ab63eefb67818246
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-08-07 17:58:33 +0400

    Add files via upload

---
+++ StartingAndStoppingProcess.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+namespace StartingAndStoppingProcess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("Notepad.exe");
+            //  Process.Start(@"C:\Users\Irakli\OneDrive\Desktop\C#");
+            //Process.Start("chrome.exe");
+            //Process.Start("https://www.facebook.com");
+
+            Process[] notepads = Process.GetProcessesByName("notepad");
+            foreach (var process in notepads)
+            {
+                process.Kill();
+            }
+        }
+    }
+}
