b1104ca7b15364f2a28fd55ce8cf597c7b26d89e
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-08-07 18:05:37 +0400

    Update StartingAndStoppingProcess.cs

--- StartingAndStoppingProcess.cs
+++ StartingAndStoppingProcess.cs
@@ -5,7 +5,12 @@ namespace StartingAndStoppingProcess
     {
         static void Main(string[] args)
         {
+            // esaa notepadis shemqmneli da teqstis chamweri. sxva videodan aq chavwere ubralod
+            System.IO.File.WriteAllText(@"C:\Users\Irakli\OneDrive\Desktop\C#\exercise.txt", "Hello papagenna");
+            System.Diagnostics.Process.Start("C:\\Users\\Irakli\\OneDrive\\Desktop\\C#");
             Process.Start("Notepad.exe");
+
+            // es dawyeba gacherebaa procesis
             //  Process.Start(@"C:\Users\Irakli\OneDrive\Desktop\C#");
             //Process.Start("chrome.exe");
             //Process.Start("https://www.facebook.com");
