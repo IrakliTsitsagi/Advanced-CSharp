cccfcf436e01bf091ac50d14fc910c68439bb302
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-08-11 14:17:58 +0400

    Add files via upload

---
+++ Indexers.cs
@@ -0,0 +1,27 @@
+﻿namespace Indexers
+{
+    internal class Indexers
+    {
+        static void Main(string[] args)
+        {
+            Animals animals = new Animals();
+
+            animals[0] = "Dog";
+            animals[1] = "Cat";
+            animals[2] = "Tiger";
+
+            Console.WriteLine(animals[1]);
+        }
+    }
+
+    class Animals
+    {
+        private string[] name = new string[100];
+
+        public string this[int index]
+        {
+            get { return name[index]; }
+            set { name[index] = value; }
+        }
+    }
+}
