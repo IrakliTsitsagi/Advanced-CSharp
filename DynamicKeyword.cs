f621883d633cb495d92c1c8df87de5daadb60d75
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-08-11 14:26:31 +0400

    Add files via upload

---
+++ DynamicKeyword.cs
@@ -0,0 +1,34 @@
+﻿namespace DynamicKeyword
+{
+    internal class DynamicKeyword
+    {
+        static void Main(string[] args)
+        {
+            dynamic value = 0;
+            Console.WriteLine("Value = " + value);
+            Console.WriteLine("value type is " + value.GetType().Name);
+            Console.WriteLine();
+
+            value = "Kata";
+            Console.WriteLine("Value = " + value);
+            Console.WriteLine("value type is " + value.GetType().Name);
+            Console.WriteLine();
+
+            value = new Animals();
+            value.Name = "Miako";
+            Console.WriteLine("Value = " + value.Name);
+            Console.WriteLine("value type is " + value.GetType().Name);
+            Console.WriteLine();
+        }
+    }
+
+    class Animals
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
