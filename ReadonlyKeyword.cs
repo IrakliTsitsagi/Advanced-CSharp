0b38ba672fc79af1cbf8b509213026022b2512e2
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-08-30 11:47:31 +0400

    Add files via upload

---
+++ ReadonlyKeyword.cs
@@ -0,0 +1,30 @@
+﻿namespace ReadonlyKeyword
+{
+    internal class ReadonlyKeyword
+    {
+        static void Main(string[] args)
+        {
+            Animals dog = new Animals("Dog");
+            Console.WriteLine(dog.AnimalName);
+
+            // ase shemidzlia shevcvalo dogis name
+            dog = new Animals("Another dog");
+            Console.WriteLine(dog.AnimalName);
+        }
+    }
+
+    class Animals
+    {
+        readonly private string animalName;
+
+        public string AnimalName
+        {
+            get => animalName;
+        }
+
+        public Animals(string name)
+        {
+            animalName = name;
+        }
+    }
+}
