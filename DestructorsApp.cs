9e233b883983a390e24fdaf9710a5f96e04b0bc2
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-08-30 12:40:49 +0400

    Add files via upload

---
+++ DestructorsApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace DestructorsApp
+{
+    internal class DestructorsApp
+    {
+        static void Main(string[] args)
+        {
+            Animals dog = new Animals();
+
+            var name = Console.ReadLine();
+
+        }
+    }
+
+    class Animals
+    {
+        public Animals()
+        {
+            Console.WriteLine("a new animal is created");
+        }
+
+        // destructor eshveba roca programa about to dasruldes
+        ~Animals()
+        {
+            Console.WriteLine("animals object is about to be destroyed");
+        }
+    }
+}
