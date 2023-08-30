8016ef2625ecc290bf1039d6ad0bd442e2361846
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-08-30 11:57:39 +0400

    Add files via upload

---
+++ StaticDirectives.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+using static System.Math;
+namespace StaticDirectives
+{
+    internal class StaticDirectives
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(Round(2.5465, 2));
+
+            // ase unda damewera imeebis gareshe using staticis
+            //  Console.WriteLine(Math.Round(2.5465, 2));
+        }
+    }
+}
