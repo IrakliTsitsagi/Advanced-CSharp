08aa29519f54891e198e68697b6a0aed272ff220
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-08-31 12:30:44 +0400

    Add files via upload

---
+++ AssembliesCultureInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Globalization;
+namespace AssembliesCultureInfo
+{
+    internal class AssembliesCultureInfo
+    {
+        static void Main(string[] args)
+        {
+            Assembly assemObject = Assembly.GetEntryAssembly();
+            AssemblyName assemName = assemObject.GetName();
+
+            Console.WriteLine(assemName.FullName);
+
+            Console.WriteLine(assemName.CultureName);
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            assemName.CultureInfo = cultureInfo;
+            Console.WriteLine(assemName.CultureInfo.DisplayName);
+            Console.WriteLine(assemName.CultureInfo.NativeName);
+            Console.WriteLine(assemName.CultureInfo.NumberFormat.CurrencySymbol);
+            Console.WriteLine(assemName.CultureInfo.Calendar);
+        }
+    }
+}
