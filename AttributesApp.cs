da348364d912d16597ba7008bfefe80e4c39be22
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-08-31 12:31:15 +0400

    Add files via upload

---
+++ AttributesApp.cs
@@ -0,0 +1,27 @@
+﻿namespace AttributesApp
+{
+    internal class AttributesApp
+    {
+        static void Main(string[] args)
+        {
+            Persons.SayHi();
+            Persons.SayHello();
+        }
+    }
+
+    class Persons
+    {
+        // say histan rom mivitan mauss qvemot mocemul teqsts michvenebs
+        [Obsolete("This method is going to be included in the upcoming versions")]
+        public static void SayHi()
+        {
+            Console.WriteLine("hi");
+        }
+
+        [Obsolete("fakiu")]
+        public static void SayHello()
+        {
+
+        }
+    }
+}
