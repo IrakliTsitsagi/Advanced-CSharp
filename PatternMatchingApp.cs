10ac9aab8802f829d08aab83f0fabd179f98d9e1
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-09-01 15:54:38 +0400

    Add files via upload

---
+++ PatternMatchingApp.cs
@@ -0,0 +1,69 @@
+﻿namespace PatternMatchingApp
+{
+    internal class PatternMatchingApp
+    {
+        static void Main(string[] args)
+        {
+            Dog iriska = new Dog();
+            iriska.DogName = "Iriska";
+            Cat kato = new Cat();
+            Duck kvato = new Duck();
+
+            AnimalSound(iriska);
+            AnimalSound(kato);
+            AnimalSound(kvato);
+        }
+
+        // using IS
+        public static void AnimalSound(object animal)
+        {
+            if (animal is Dog)
+                Console.WriteLine("Woof");
+            if (animal is Cat)
+                Console.WriteLine("Meow");
+            if (animal is Duck)
+                Console.WriteLine("Quack");
+        }
+
+        // using SWITCH
+        public static void AnimalSoundWithSwitch(object animal)
+        {
+            switch (animal)
+            {
+                case Dog d: Console.WriteLine("woof"); break;
+                case Cat d: Console.WriteLine("miau"); break;
+                case Duck d: Console.WriteLine("yiyiyi"); break;
+                default:
+                    Console.WriteLine("xz"); break;
+            }
+        }
+        // using SWITCH and WHEN
+
+        public static void AnimalSoundWithSwitchAndWhen(object animal)
+        {
+            switch (animal)
+            {
+                case Dog d when d.DogName == null: Console.WriteLine("name is not set"); break;
+
+            }
+        }
+    }
+
+    class Dog
+    {
+        public string DogSound { get; set; }
+        public string DogName { get; set; }
+    }
+
+    class Cat
+    {
+        public string CatSound { get; set; }
+        public string CatName { get; set; }
+    }
+
+    class Duck
+    {
+        public string DuckSound { get; set; }
+        public string DuckName { get; set; }
+    }
+}
