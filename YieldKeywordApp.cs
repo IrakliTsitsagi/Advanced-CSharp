04df799e205db17175187a46729663114b08bcc3
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-09-04 15:17:09 +0400

    Add files via upload

---
+++ YieldKeywordApp.cs
@@ -0,0 +1,22 @@
+﻿namespace YieldKeywordApp
+{
+    internal class YieldKeywordApp
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(PerformMathOperations(2,3));
+            foreach (var item in PerformMathOperations(3, 2))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static IEnumerable<double> PerformMathOperations(int numOne, int numTwo)
+        {
+            yield return numOne + numTwo;  // yeldis da ienumerables gareshe 2 jer ver daareturneb
+            yield return numOne * numTwo;
+            yield return numOne - numTwo;
+            yield return Math.Round(((double)numOne / (double)numTwo), 2);
+        }
+    }
+}
