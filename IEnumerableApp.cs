a030312adf268d5a47a70ea53267a5cdf3bca356
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-09-04 14:28:09 +0400

    Add files via upload

---
+++ IEnumerableApp.cs
@@ -0,0 +1,50 @@
+﻿namespace IEnumerableApp
+{
+    internal class IEnumerableApp
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> result = from value in Enumerable.Range(1, 10)
+                                      select value;
+
+            foreach (int value in result)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine();
+
+            double avg = result.Average();
+            Console.WriteLine(avg);
+
+            List<int> list = result.ToList();
+            int[] array = result.ToArray();
+
+            list.Add(11);
+            list.Add(12);
+            list.Add(13);
+            list.Add(14);
+            list.Add(15);
+            Console.WriteLine("What is in the list now");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            result = list.AsEnumerable(); // amis gareshe ienumerable 10 mde iqneboda iseva
+            Console.WriteLine("What is in the ienumerable now");
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------");
+
+            IEnumerator<int> enumerator = list.GetEnumerator();
+
+            while (enumerator.MoveNext()) // foreach uarze iyo da viyenebt whiles
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+}
