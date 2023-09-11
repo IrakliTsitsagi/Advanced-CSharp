9831c9b16dbe9e7c0490bde50a9bc9f1b8f2471c
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-09-11 12:19:26 +0400

    Add files via upload

---
+++ NullCoalescingAssignmentNewC.cs
@@ -0,0 +1,21 @@
+﻿namespace NullCoalescingAssignmentNewC
+{
+    internal class NullCoalescingAssignmentNewC
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int? i = null;
+
+            numbers.Add(i ??= 5); // if null mashin 5 gaxdeba tu araa null da magalitad
+            // zemot weria ro int i = 2 mashin ar gaxdeba 5
+
+            Console.WriteLine("Value of i = " + i);
+
+            foreach (var no in numbers)
+            {
+                Console.WriteLine(no);
+            }
+        }
+    }
+}
