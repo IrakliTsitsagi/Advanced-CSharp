b8ddf98fa0ee021808fb155aa16e7ccda73ac0fc
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-09-11 12:04:48 +0400

    Add files via upload

---
+++ DefaultInterfaceAppNewC.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Channels;
+
+namespace DefaultInterfaceAppNewC
+{
+    internal class DefaultInterfaceAppNewC
+    {
+        static void Main(string[] args)
+        {
+            ICar F1 = new Ferrari();
+            F1.Start();
+            F1.Run();
+
+            Ferrari Spider = new Ferrari();
+            ((ICar)Spider).Run(200);
+        }
+    }
+
+    class Ferrari : ICar
+    {
+        public void Run()
+        {
+            Console.WriteLine("Car is running");
+        }
+    }
+
+    interface ICar
+    {
+        public void Start() => Console.WriteLine("Car has started!");
+
+        public void Run();
+
+        public void Run(double carSpeed) => Console.WriteLine($"Car is runing at {carSpeed} km/h");
+    }
+}
