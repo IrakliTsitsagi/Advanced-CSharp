645ff8b6e7cd0ec91f707c477a2d20f06c3c1408
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-09-11 14:21:05 +0400

    Add files via upload

---
+++ DisposableRefStructsNewC.cs
@@ -0,0 +1,27 @@
+﻿namespace DisposableRefStructsNewC
+{
+    internal class DisposableRefStructsNewC
+    {
+        static void Main(string[] args)
+        {
+            using (var car = new Car())
+            {
+                Console.WriteLine("My car is very cool");
+            }
+        }
+    }
+
+    // ese erors agdebs
+    //ref struct Car : IDisposable
+    //{
+
+    //}
+    // davfixet usingis mainshi gamoyenebita
+    ref struct Car
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose Method is Called");
+        }
+    }
+}
