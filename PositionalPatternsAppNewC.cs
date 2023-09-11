14881bec55c352158ee4356af9e4ccb45292f058
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-09-11 15:34:37 +0400

    Add files via upload

---
+++ PositionalPatternsAppNewC.cs
@@ -0,0 +1,32 @@
+﻿namespace PositionalPatternsAppNewC
+{
+
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y) => (X, Y) = (x, y);
+
+        public void Deconstruct(out int x, out int y) =>
+            (x, y) = (X, Y);
+    }
+    internal class PositionalPatternsAppNewC
+    {
+        static void Main(string[] args)
+        {
+            Point startPoint = new Point(0, 0);
+            Console.WriteLine(DisplayPosition(startPoint));
+            Point point = new Point(100, 150);
+            Console.WriteLine(DisplayPosition(point));
+        }
+
+        static string DisplayPosition(Point point) =>
+            point switch
+            {
+                (0, 0) => "Origin",
+                var (x, y) when x > 0 && y > 0 => $"Current position is {x}, {y}",
+                _ => "unknown position" // _=> es nishnavs defaults
+            };
+    }
+}
