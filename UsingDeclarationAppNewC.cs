7d123656474e422e092517fda9e4292c0c357dcc
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-09-11 12:32:06 +0400

    Add files via upload

---
+++ UsingDeclarationAppNewC.cs
@@ -0,0 +1,30 @@
+﻿namespace UsingDeclarationAppNewC
+{
+    internal class UsingDeclarationAppNewC
+    {
+        class Resource : IDisposable
+        {
+            public void Use() => Console.WriteLine("Using resource...");
+            public void Dispose() => Console.WriteLine("Disposing resource...");
+        }
+        static void Main(string[] args)
+        {
+            //var resourse = new Resource();
+            //resourse.Use();
+            //Console.WriteLine("\nThis app is awesome");
+            //resourse.Dispose();
+
+            // better version
+            // using var resource = new Resource();
+            //resource.Use();
+            //Console.WriteLine("\nThis app is awesome");
+
+            // and also better version but different
+            using (var resource = new Resource())
+            {
+                resource.Use();
+            }
+            Console.WriteLine("\nThis app is awesome");
+        }
+    }
+}
