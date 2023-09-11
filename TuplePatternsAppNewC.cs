a9f5ddd2dc5da24d7a39c86217539cd96edecef4
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-09-11 15:24:29 +0400

    Add files via upload

---
+++ TuplePatternsAppNewC.cs
@@ -0,0 +1,32 @@
+﻿namespace TuplePatternsAppNewC
+{
+    internal class TuplePatternsAppNewC
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckPrices("Vienna", "Austria"));
+            Console.WriteLine(CheckPrices("Paris", "France"));
+            Console.WriteLine(CheckPrices("Paris", "USA"));
+
+            Console.WriteLine(CheckPrices("Vienna", "Austria", "German"));
+        }
+
+        public static string CheckPrices(string city, string country, string language) =>
+            (city, country, language) switch
+            {
+                ("Vienna", "Austria", "German") => "Above average",
+                ("Paris", "France", "French") => "Very High",
+                ("Paris", "USA", "English") => "Average",
+                (_, _, _) => " no info"
+            };
+
+        public static string CheckPrices(string city, string country) =>
+            (city, country) switch
+            {
+                ("Vienna", "Austria") => "Above Average",
+                ("Paris", "France") => "Very High",
+                ("Paris", "USA") => "Average",
+                (_, _) => " no info"
+            };
+    }
+}
